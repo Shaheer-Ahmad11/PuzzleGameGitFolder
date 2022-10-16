9c4e6ff398340bde5edf7d8eb7192c6a34b7befb
muhammadHamza8066 <112260370+muhammadHamza8066@users.noreply.github.com>
2022-10-16 14:30:32 +0500

    puzzelgame p1

---
+++ PuzzleGame/Assets/_GameData/Scripts/gameScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameScript : MonoBehaviour
+{
+    [SerializeField] private Transform emptySpace=null;
+    private Camera _camera;
+    void Start()
+    {
+        _camera=Camera.main;
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+
+            Ray ray=_camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit= Physics2D.Raycast(ray.origin,ray.direction);
+
+            if(hit){
+                if(Vector2.Distance(a:emptySpace.position, b:hit.transform.position)<2){
+                    Vector2 lastEmptySpacePos=emptySpace.position;
+                    emptySpace.position=hit.transform.position;
+                    hit.transform.position=lastEmptySpacePos;
+
+
+                }
+
+            }
+        } 
+    }
+}
