4656aa068a7b84cfad1fc9961ebb253fbe46dbb7
MAbdullahNA <102964719+MAbdullahNA@users.noreply.github.com>
2022-11-01 02:03:45 +0500

    green tick btns, UI setting

--- PuzzleGame/Assets/Find The Differences/Scripts/CameraAspect.cs
+++ PuzzleGame/Assets/Find The Differences/Scripts/CameraAspect.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class CameraAspect : MonoBehaviour
 {
-	private void Start()
-	{
-		Camera.main.aspect = 0.625f;
-	}
+    private void Start()
+    {
+        // Camera.main.aspect = 0.625f;
+        Camera.main.aspect = 0.56f;
+    }
 
-	private void Update()
-	{
-	}
+    private void Update()
+    {
+    }
 }
